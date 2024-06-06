a681459136eefb9d4fb34cbc9da3965700c68b33
Pats03 <ananthateja2004@gmail.com>
2024-06-06 14:14:34 +0530

    new file:   client/.eslintrc.cjs 	new file:   client/.gitignore 	new file:   client/README.md 	new file:   client/index.html 	new file:   client/package-lock.json 	new file:   client/package.json 	new file:   client/public/apple-touch-icon.png 	new file:   client/public/favicon.ico 	new file:   client/src/App.jsx 	new file:   client/src/assets/css/index.css 	new file:   client/src/assets/images/avatar-1.jpg 	new file:   client/src/assets/images/avatar-2.jpg 	new file:   client/src/assets/images/book.svg 	new file:   client/src/assets/images/favicon.ico 	new file:   client/src/assets/images/kitchen.jpg 	new file:   client/src/assets/images/logo.svg 	new file:   client/src/assets/images/main-alternative.svg 	new file:   client/src/assets/images/main.svg 	new file:   client/src/assets/images/not-found.svg 	new file:   client/src/assets/react.svg 	new file:   client/src/assets/wrappers/BigSidebar.js 	new file:   client/src/assets/wrappers/ChartsContainer.js 	new file:   client/src/assets/wrappers/Dashboard.js 	new file:   client/src/assets/wrappers/DashboardFormPage.js 	new file:   client/src/assets/wrappers/ErrorPage.js 	new file:   client/src/assets/wrappers/Job.js 	new file:   client/src/assets/wrappers/JobInfo.js 	new file:   client/src/assets/wrappers/JobsContainer.js 	new file:   client/src/assets/wrappers/LandingPage.js 	new file:   client/src/assets/wrappers/LogoutContainer.js 	new file:   client/src/assets/wrappers/Navbar.js 	new file:   client/src/assets/wrappers/PageBtnContainer.js 	new file:   client/src/assets/wrappers/RegisterAndLoginPage.js 	new file:   client/src/assets/wrappers/SmallSidebar.js 	new file:   client/src/assets/wrappers/StatItem.js 	new file:   client/src/assets/wrappers/StatsContainer.js 	new file:   client/src/assets/wrappers/Testing.js 	new file:   client/src/assets/wrappers/ThemeToggle.js 	new file:   client/src/components/BigSidebar.jsx 	new file:   client/src/components/LogoutContainer.jsx 	new file:   client/src/components/NavLinks.jsx 	new file:   client/src/components/Navbar.jsx 	new file:   client/src/components/SmallSidebar.jsx 	new file:   client/src/components/ThemeToggle.jsx 	new file:   client/src/components/formrow.jsx 	new file:   client/src/components/index.js 	new file:   client/src/components/logo.jsx 	new file:   client/src/index.css 	new file:   client/src/main.jsx 	new file:   client/src/pages/Addsub.jsx 	new file:   client/src/pages/Admin.jsx 	new file:   client/src/pages/Allsubs.jsx 	new file:   client/src/pages/DashboardLayout.jsx 	new file:   client/src/pages/EditSubject.jsx 	new file:   client/src/pages/Error.jsx 	new file:   client/src/pages/Landing.jsx 	new file:   client/src/pages/Login.jsx 	new file:   client/src/pages/Stats.jsx 	new file:   client/src/pages/db.jsx 	new file:   client/src/pages/homelayout.jsx 	new file:   client/src/pages/index.js 	new file:   client/src/pages/profile.jsx 	new file:   client/src/pages/register.jsx 	new file:   client/src/utils/links.jsx 	new file:   client/vite.config.js 	new file:   client/vite.svg 	modified:   server/routes/routes.js 	modified:   server/schema/schema.js 	modified:   server/test.rest

---
+++ client/src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Logo } from './logo';
+export { default as Formrow } from './formrow';
+export { default as BigSidebar } from './BigSidebar';
+export { default as Navbar } from './Navbar';
+export { default as SmallSidebar } from './SmallSidebar';
