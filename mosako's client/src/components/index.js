4bfba4d63101b8558e9fd6769b17920ac8e648ca
NishanthChowdary097 <nishanthchowdary118@gmail.com>
2024-07-09 13:40:38 +0530

    new file:   .gitignore 	deleted:    mosako's client/.eslintrc.cjs 	deleted:    mosako's client/.gitignore 	deleted:    mosako's client/README.md 	deleted:    mosako's client/index.html 	deleted:    mosako's client/package-lock.json 	deleted:    mosako's client/package.json 	deleted:    mosako's client/postcss.config.js 	deleted:    mosako's client/public/vite.svg 	deleted:    mosako's client/src/App.css 	deleted:    mosako's client/src/App.jsx 	deleted:    mosako's client/src/assets/react.svg 	deleted:    mosako's client/src/components/Loader.jsx 	deleted:    mosako's client/src/components/Maincontent.jsx 	deleted:    mosako's client/src/components/Navbar.jsx 	deleted:    mosako's client/src/components/Searchpage.jsx 	deleted:    mosako's client/src/components/Sidebar.jsx 	deleted:    mosako's client/src/components/index.js 	deleted:    mosako's client/src/index.css 	deleted:    mosako's client/src/main.jsx 	deleted:    mosako's client/src/pages/Error.jsx 	deleted:    mosako's client/src/pages/Home.jsx 	deleted:    mosako's client/src/pages/HomeLayout.jsx 	deleted:    mosako's client/src/pages/index.js 	deleted:    mosako's client/tailwind.config.js 	deleted:    mosako's client/vite.config.js

--- mosako's client/src/components/index.js
+++
@@ -1,4 +0,0 @@
-export {default as Navbar} from './Navbar';
-export {default as Sidebar} from './Sidebar';
-export {default as MainContent} from './Maincontent';
-export {default as SearchPage} from './Searchpage';
